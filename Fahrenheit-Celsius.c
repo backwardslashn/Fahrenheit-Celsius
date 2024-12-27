5f6ad116f62584c2c064df37501aaa8f5dae3015
backwardslashn <backwardslashn@gmail.com>
2024-12-27 04:22:21 -0800

    Update Fahrenheit-Celsius.c

--- Fahrenheit-Celsius.c
+++ Fahrenheit-Celsius.c
@@ -8,6 +8,8 @@
     for fahr = 0, 20, .... 300; floating-point version */
 int main()
 {
+    printf("Fahrenhit-Celsius table\n");
+
     float fahr, celsius;
     int lower, upper, step;
 
@@ -18,7 +20,7 @@ int main()
     fahr = lower;
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
-        printf("%3.0f %6.1f\n", fahr, celsius);
+        printf("%3.0f %11.1f\n", fahr, celsius);
         fahr = fahr + step;
     }
 }
