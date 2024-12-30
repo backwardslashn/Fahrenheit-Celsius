32f9fc8b49141f7a8fcb910b8650511890dafcb9
backwardslashn <backwardslashn@gmail.com>
2024-12-30 03:07:43 -0800

    Update For-Fahrenheit-Celsius.c
    It's bad practice to bury "magic number like 300 & 20" in the program they convey little information to someone who might have to read the program later

--- For-Fahrenheit-Celsius.c
+++ For-Fahrenheit-Celsius.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /*
-version 1.0.0
+version 1.0.1
 */
+
+#define LOWER 0     /* lower limit of table */
+#define UPPER 300   /* upper limit */
+#define STEP 20     /* step size */
 
 int main()
 {
     int fahr;
 
-    for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
         printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
 }
